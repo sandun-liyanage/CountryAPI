0c166a6b292d1e8c6ac195211beb7acb0e77e39e
Sandun Liyanage <sandunudesh03@gmail.com>
2025-08-02 17:48:31 +0530

    Initialized the project and Configured project structure.

---
+++ CountryAPI/Program.cs
@@ -0,0 +1,31 @@
+using RepositoryLayer.Repositories;
+using ServiceLayer.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+string? connStr = Environment.GetEnvironmentVariable("COUNTRY_DB_CONN")
+    ?? builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddScoped<ICountryService, CountryService>();
+builder.Services.AddScoped<ICountryRepository, CountryRepository>();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
